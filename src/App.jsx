5297bf2fe087345f895ba1728b61ef0c30d6d369
Corrie Grimshaw <corrie.e.grimshaw@gmail.com>
2019-03-23 12:11:55 -0700

    Update client to receive Bookings

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const fetchURL = 'http://localhost:3838/bookings'
+
+
+
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      bookings: null
+    }
+  }
+  componentDidMount(){
+    console.log('mounted')
+    fetch(fetchURL).then((res)=>{
+      res.json().then((data)=>{
+        console.log(data);
+      })
+    })
+  }
 
   render(){
-    return <div>it's an app all right</div>
+    return <div>{this.state.bookings}</div>
   }
 }
 
